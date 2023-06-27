fb9b927ecbdcddd3667f1c17c216932ed1bbee07
Hoang <nguyendiephoang111@gmail.com>
2023-06-27 22:27:19 +0700

    Combining Reducers

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -7,7 +7,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
 // function Contact(props) {
 
-class Contact extends Component {
+class Contact extends Component {   
     constructor(props) {
         super(props);
 
@@ -88,15 +88,15 @@ class Contact extends Component {
 
 
     render() {
-        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+        // const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
         return (
             <div className="container">
-
+                  
                 <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
                     <Row className="form-group">
                         <Label htmlFor="firstname" md={2}>First Name</Label>    
                         <Col md={10}>
-                            <Control.text model=".firstname" id="firstname" name="firstname"
+                            <Control.Text model=".firstname" id="firstname" name="firstname"
                                 placeholder="First Name"
                                 className="form-control"
                             />
@@ -105,7 +105,7 @@ class Contact extends Component {
                     <Row className="form-group">
                         <Label htmlFor="lastname" md={2}>Last Name</Label>
                         <Col md={10}>
-                            <Control.text model=".lastname" id="lastname" name="lastname"
+                            <Control.Text model=".lastname" id="lastname" name="lastname"
                                 placeholder="Last Name"
                                 className="form-control"
                             />
@@ -114,7 +114,7 @@ class Contact extends Component {
                     <Row className="form-group">
                         <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
                         <Col md={10}>
-                            <Control.text model=".telnum" id="telnum" name="telnum"
+                            <Control.Text model=".telnum" id="telnum" name="telnum"
                                 placeholder="Tel. Number"
                                 className="form-control"
                             />
@@ -123,7 +123,7 @@ class Contact extends Component {
                     <Row className="form-group">
                         <Label htmlFor="email" md={2}>Email</Label>
                         <Col md={10}>
-                            <Control.text model=".email" id="email" name="email"
+                            <Control.Text model=".email" id="email" name="email"
                                 placeholder="Email"
                                 className="form-control" />
                         </Col>
@@ -132,7 +132,7 @@ class Contact extends Component {
                         <Col md={{ size: 6, offset: 2 }}>
                             <div className="form-check">
                                 <Label check>
-                                    <Control.checkbox model=".agree" name="agree"
+                                    <Control.Checkbox model=".agree" name="agree"
                                         className="form-check-input"
                                     /> {' '}
                                     <strong>May we contact you?</strong>
@@ -140,17 +140,17 @@ class Contact extends Component {
                             </div>
                         </Col>
                         <Col md={{ size: 3, offset: 1 }}>
-                            <Control.select model=".contactType" name="contactType"
+                            <Control.Select model=".contactType" name="contactType"
                                 className="form-control">
                                 <option>Tel.</option>
                                 <option>Email</option>
-                            </Control.select>
+                            </Control.Select>
                         </Col>
                     </Row>
                     <Row className="form-group">
                         <Label htmlFor="message" md={2}>Your Feedback</Label>
                         <Col md={10}>
-                            <Control.textarea model=".message" id="message" name="message"
+                            <Control.Textarea model=".message" id="message" name="message"
                                 rows="12"
                                 className="form-control" />
                         </Col>
@@ -202,7 +202,7 @@ class Contact extends Component {
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
                             <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                            <a role="button" className="btn btn-info" href="#"><i className="fa fa-skype" ></i> Skype</a>
+                            <a role="button" className="btn btn-info" href="tel:+85212345678"><i className="fa fa-skype" ></i> Skype</a>
                             <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
                     </div>
