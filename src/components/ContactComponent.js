78b0b6ffed20a9c9250f0ec0c124e970c44a0452
Hoang <nguyendiephoang111@gmail.com>
2023-06-30 10:28:02 +0700

    React Redux Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, Row, FormFeedback } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { connect } from 'react-redux';
 
 
 
@@ -87,82 +88,85 @@ class Contact extends Component {
     }
 
 
+
     render() {
-        // const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+         const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
         return (
             <div className="container">
                   
-                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                    <Row className="form-group">
-                        <Label htmlFor="firstname" md={2}>First Name</Label>    
-                        <Col md={10}>
-                            <Control.Text model=".firstname" id="firstname" name="firstname"
-                                placeholder="First Name"
-                                className="form-control"
-                            />
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Label htmlFor="lastname" md={2}>Last Name</Label>
-                        <Col md={10}>
-                            <Control.Text model=".lastname" id="lastname" name="lastname"
-                                placeholder="Last Name"
-                                className="form-control"
-                            />
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
-                        <Col md={10}>
-                            <Control.Text model=".telnum" id="telnum" name="telnum"
-                                placeholder="Tel. Number"
-                                className="form-control"
-                            />
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Label htmlFor="email" md={2}>Email</Label>
-                        <Col md={10}>
-                            <Control.Text model=".email" id="email" name="email"
-                                placeholder="Email"
-                                className="form-control" />
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Col md={{ size: 6, offset: 2 }}>
-                            <div className="form-check">
-                                <Label check>
-                                    <Control.Checkbox model=".agree" name="agree"
-                                        className="form-check-input"
-                                    /> {' '}
-                                    <strong>May we contact you?</strong>
-                                </Label>
-                            </div>
-                        </Col>
-                        <Col md={{ size: 3, offset: 1 }}>
-                            <Control.Select model=".contactType" name="contactType"
-                                className="form-control">
-                                <option>Tel.</option>
-                                <option>Email</option>
-                            </Control.Select>
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Label htmlFor="message" md={2}>Your Feedback</Label>
-                        <Col md={10}>
-                            <Control.Textarea model=".message" id="message" name="message"
-                                rows="12"
-                                className="form-control" />
-                        </Col>
-                    </Row>
-                    <Row className="form-group">
-                        <Col md={{ size: 10, offset: 2 }}>
-                            <Button type="submit" color="primary">
-                                Send Feedback
-                            </Button>
-                        </Col>
-                    </Row>
-                </LocalForm>
+               
+                  <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+<Row className="form-group">
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".firstname" id="firstname" name="firstname"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".lastname" id="lastname" name="lastname"
+                                        placeholder="Last Name"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                                <Col md={10}>
+                                    <Control.text model=".telnum" id="telnum" name="telnum"
+                                        placeholder="Tel. Number"
+                                        className="form-control"
+                                         />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Control.text model=".email" id="email" name="email"
+                                        placeholder="Email"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size: 6, offset: 2}}>
+                                    <div className="form-check">
+                                        <Label check>
+                                            <Control.checkbox model=".agree" name="agree"
+                                                className="form-check-input"
+                                                 /> {' '}
+                                                <strong>May we contact you?</strong>
+                                        </Label>
+                                    </div>
+                                </Col>
+                                <Col md={{size: 3, offset: 1}}>
+                                    <Control.select model=".contactType" name="contactType"
+                                        className="form-control">
+<option>Tel.</option>
+                                        <option>Email</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="12"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                    Send Feedback
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                        
 
 
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect,withRouter} from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import {withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+
 
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
-    comments: state.comments, 
+    comments: state.comments,
     promotions: state.promotions,
     leaders: state.leaders
   }
@@ -66,7 +67,7 @@ class Main extends Component {
         <div>
           <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <Main leaders={this.props.leaders} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
